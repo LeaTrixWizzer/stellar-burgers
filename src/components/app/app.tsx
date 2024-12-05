c193d8e4560b573e6afd91e4f4ad8863a1ee65bf
Anastasia <leatrix5@yandex.ru>
2024-12-05 18:36:10 +0400

    add routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,46 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, OrderInfo } from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      // todo protected
+      <Route path='/register' element={<Register />} />
+      // todo protected
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      // todo protected
+      <Route path='/reset-password' element={<ResetPassword />} />
+      // todo protected
+      <Route path='/profile' element={<Profile />} />
+      // todo: protected
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      // todo protected
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/feed/:number' element={<OrderInfo />} />
+      // todo modal
+      <Route path='/ingredients/:id' element={<IngredientDetails />} />
+      // todo modal
+      <Route path='/profile/orders/:number' element={<OrderInfo />} />
+      // todo protected and modal
+    </Routes>
   </div>
 );
 
